85fdfcc64e01b9ad9eb29e47421b93ab5d560bb9
Jerome Etienne <jerome.etienne@gmail.com>
2011-12-17 13:10:51 +0100

    threex.sparks.js typo

--- threex.sparks.js
+++ threex.sparks.js
@@ -172,7 +172,7 @@ THREEx.Sparks.vertexShaderText	= [
 	"varying	vec4	vColor;",
 
 	"void main() {",
-		"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
+		"vec4 mvPosition= modelViewMatrix * vec4( position, 1.0 );",
 		"gl_PointSize	= size * sizeRatio * ( 150.0 / length( mvPosition.xyz ) );",
 		"gl_Position	= projectionMatrix * mvPosition;",
 
