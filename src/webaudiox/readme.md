288230d3b58406b6803d900e021aadad3b635fa2
Jerome Etienne <jerome.etienne@gmail.com>
2013-06-07 02:20:20 +0200

    more work

--- src/webaudiox/readme.md
+++ src/webaudiox/readme.md
@@ -2,3 +2,11 @@ webaudiox.js
 ============
 it is a webaudio api ala vendor.js.
 no libs only snippets for you to copy, and fine tune to your own needs.
+
+## TODO
+* port all the three.js localisation
+  * webaudiox.three.js
+  * put a panner
+  * put some dopler
+* put in its own repo
+* port examples from webaudio.js
---
+++ src/webaudiox/webaudiox.jsfx.js
@@ -0,0 +1,10 @@
+WebAudiox.prototype.bufferFromJsfx	= function(lib){
+	var params	= jsfxlib.arrayToParams(lib);
+	var data	= jsfx.generate(params);
+	var buffer	= this.context.createBuffer(1, data.length, 44100);
+	var fArray	= buffer.getChannelData(0);
+	for(var i = 0; i < fArray.length; i++){
+		fArray[i]	= data[i];
+	}
+	return buffer;
+}
