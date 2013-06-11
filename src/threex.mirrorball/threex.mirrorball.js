09d4a349cec08b322898034d6122b5c54ccde8bf
Jerome Etienne <jerome.etienne@gmail.com>
2013-06-11 13:06:13 +0200

    more work

--- src/threex.mirrorball/threex.mirrorball.js
+++ src/threex.mirrorball/threex.mirrorball.js
@@ -5,25 +5,25 @@ THREEx.MirrorBall	= function(textureW){
 	textureW	= textureW || 1024;
 		
 	// create container
-	var object	= new THREE.Object3D()
-	this.object	= object
+	var object3d	= new THREE.Object3D()
+	this.object3d	= object3d
 
 	// create the camera
 	var camera	= new THREE.CubeCamera( 0.001, 1000, textureW );
 	this.camera	= camera
 	// to avoid flickering on the border of the sphere
 	camera.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
-	object.add(camera)
+	object3d.add(camera)
 	// create the ball itself
 	var geometry	= new THREE.SphereGeometry(0.5, 32, 16)
-	var material	= new THREE.MeshBasicMaterial()
+	var material	= new THREE.MeshPhongMaterial()
 	material.envMap	= camera.renderTarget
 	material.color.set('gold')
 
 	var sphere	= new THREE.Mesh(geometry, material)
 	this.sphere	= sphere
 
-	object.add(sphere)
+	object3d.add(sphere)
 }
 
 THREEx.MirrorBall.prototype.update	= function(renderer, scene){
