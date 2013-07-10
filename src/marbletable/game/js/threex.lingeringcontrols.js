814799d74b6496b65011977edc975bdc63917d92
Jerome Etienne <jerome.etienne@gmail.com>
2013-07-10 22:34:59 +0200

    better handling of level

--- src/marbletable/game/js/threex.lingeringcontrols.js
+++ src/marbletable/game/js/threex.lingeringcontrols.js
@@ -1,22 +1,24 @@
 var THREEx	= THREEx	|| {}
 
-THREEx.LingeringControls	= function(controlled, centerObject, radius){
-	var centerPos		= new THREE.Vector3()
-	var controlledPos	= new THREE.Vector3()
+THREEx.LingeringControls	= function(object3d, target, radius){
+	var targetPosition	= new THREE.Vector3()
+	var object3dPosition	= new THREE.Vector3()
 	var deltaPosition	= new THREE.Vector3()
+	var previousAngle	= null
 	this.update	= function(delta, now){
-		controlled.updateMatrixWorld();
-		controlledPos.getPositionFromMatrix( controlled.matrixWorld )
-		
-		centerObject.updateMatrixWorld();
-		centerPos.getPositionFromMatrix( centerObject.matrixWorld )
-		
-		deltaPosition.subVectors(controlledPos, centerPos)
-		var angle	= Math.atan2(deltaPosition.z, deltaPosition.x)		
-		
-		controlled.position.x	= centerPos.x + Math.cos(angle)*radius
-		controlled.position.z	= centerPos.z + Math.sin(angle)*radius
-		
-		controlled.lookAt(centerPos)
+		// get world position of object3d
+		object3d.updateMatrixWorld();
+		object3dPosition.getPositionFromMatrix( object3d.matrixWorld )
+		// get world position of target		
+		target.updateMatrixWorld();
+		targetPosition.getPositionFromMatrix( target.matrixWorld )
+		// get angle between object3d and target
+		deltaPosition.subVectors(object3dPosition, targetPosition)
+		var angle	= Math.atan2(deltaPosition.z, deltaPosition.x)
+		// force distance between object3d and target to be radius
+		object3d.position.x	= targetPosition.x + Math.cos(angle)*radius
+		object3d.position.z	= targetPosition.z + Math.sin(angle)*radius
+		// set object3d direction
+		object3d.lookAt(targetPosition)
 	}
 }
