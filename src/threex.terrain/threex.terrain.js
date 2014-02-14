1115ed138d5a5a9ebf3e3af67181ccc3bd44e1a4
Jerome Etienne <jerome.etienne@gmail.com>
2014-02-14 10:53:56 +0100

    more work

--- src/threex.terrain/threex.terrain.js
+++ src/threex.terrain/threex.terrain.js
@@ -89,8 +89,57 @@ THREEx.Terrain.heightMapToPlaneGeometry	= function(heightMap){
 		}
 	}
 	geometry.verticesNeedUpdate	= true
+
+geometry.computeCentroids()
+geometry.computeTangents()
+geometry.tangentsNeedUpdate	= true
+geometry.elementsNeedUpdate	= true
 	geometry.computeFaceNormals()
 	geometry.computeVertexNormals()
 	geometry.normalsNeedUpdate	= true
 	return geometry
+}
+
+THREEx.Terrain.heightMapToVertexColor	= function(heightMap, geometry){
+	// get heightMap dimensions
+	var width	= heightMap.length
+	var depth	= heightMap[0].length
+	// loop on each vertex of the geometry
+	var color	= new THREE.Color()
+	for(var i = 0; i < geometry.faces.length; i++){
+		var face	= geometry.faces[i]
+		console.assert(face instanceof THREE.Face4)
+		face.vertexColors.push( vertexIdxToColor(face.a).clone() )
+		face.vertexColors.push( vertexIdxToColor(face.b).clone() )
+		face.vertexColors.push( vertexIdxToColor(face.c).clone() )
+		face.vertexColors.push( vertexIdxToColor(face.d).clone() )
+	}
+	geometry.colorsNeedUpdate	= true
+	return
+	
+	function vertexIdxToColor(vertexIdx){
+		var x	= Math.floor(vertexIdx % width)
+		var z	= Math.floor(vertexIdx / width)
+		return coordToColor(x, z)
+	}
+
+	function coordToColor(x, z){	
+		var height	= heightMap[x][z]
+
+		// compute color based on height
+		if( height < 0.5 ){
+			height		= (height*2)*0.5 + 0.2
+			color.setRGB(0,0, height)
+		}else if( height < 0.7 ){
+			height		= (height-0.5)/0.2
+			height		= height*0.5 + 0.2
+			color.setRGB(0,height, 0)
+		}else{
+			height		= (height-0.7)/0.3
+			height		= height*0.5 + 0.5
+			color.setRGB(height,height, height)
+		}
+		// color.setRGB(1,1,1)
+		return color;
+	}
 }
