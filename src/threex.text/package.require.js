7abcb90dd171d0a94a435b7efca7f49d94581640
Jerome Etienne <jerome.etienne@gmail.com>
2014-02-20 18:47:41 +0100

    initial threex.text

---
+++ src/threex.text/package.require.js
@@ -0,0 +1,13 @@
+define( [ './threex.text'
+	, './fonts/droid/droid_serif_regular.typeface'
+	, './fonts/gentilis_bold.typeface'
+	, './fonts/gentilis_regular.typeface'
+	, './fonts/optimer_bold.typeface'
+	, './fonts/optimer_regular.typeface'
+	, './fonts/helvetiker_bold.typeface'
+	, './fonts/helvetiker_regular.typeface'
+	, './fonts/droid/droid_sans_regular.typeface'
+	, './fonts/droid/droid_sans_bold.typeface'
+	, './fonts/droid/droid_serif_bold.typeface'
+	], function(){
+})
