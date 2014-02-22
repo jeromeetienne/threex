532de3330091130cf22019c28e59304610083c42
Jerome Etienne <jerome.etienne@gmail.com>
2014-02-22 21:34:45 +0100

    more work

--- src/threex.domevents/threex.linkify.js
+++ src/threex.domevents/threex.linkify.js
@@ -1,7 +1,8 @@
 var THREEx	= THREEx	|| {}
 
 
-THREEx.linkify	= function(domEvents, mesh, url){
+THREEx.Linkify	= function(domEvents, mesh, url, withBoundingBox){
+	withBoundingBox	= withBoundingBox !== undefined ? withBoundingBox : true
 	// compute geometry size
 	var geometry	= mesh.geometry
 	geometry.computeBoundingBox();
@@ -9,12 +10,23 @@ THREEx.linkify	= function(domEvents, mesh, url){
 	size.x	= (geometry.boundingBox.max.x - geometry.boundingBox.min.x)
 	size.y	= (geometry.boundingBox.max.y - geometry.boundingBox.min.y)
 	size.z	= (geometry.boundingBox.max.z - geometry.boundingBox.min.z)
+	
+	// create the boundingBox if needed
+	if( withBoundingBox ){
+		var boundingBox	= new THREE.Mesh(new THREE.CubeGeometry(1,1,1), new THREE.MeshBasicMaterial({
+			wireframe	: true
+		}))
+		boundingBox.visible	= false
+		boundingBox.scale.copy(size)
+		mesh.add(boundingBox)	
+	}
 
 	// build the underline
 	var underlineH	= size.y / 10;
 	var deltaY	= size.y / 20;
 	var underline	= new THREE.Mesh(new THREE.CubeGeometry(size.x, underlineH, size.z), new THREE.MeshNormalMaterial())
 	underline.position.y	+= -size.y/2 - deltaY - underlineH/2
+	this.underline	= underline
 
 	// make it invisible by default
 	underline.visible	= false;
@@ -23,19 +35,25 @@ THREEx.linkify	= function(domEvents, mesh, url){
 	mesh.add(underline)
 
 	// bind the click
-	domEvents.bind(mesh, 'click', function(event){
+	var eventTarget	= withBoundingBox ? boundingBox : mesh 
+	this.eventTarget= eventTarget
+	domEvents.bind(eventTarget, 'click', function(event){
 		window.open(url, '_blank');
 	});
 
 	// bind 'mouseover'
-	domEvents.bind(mesh, 'mouseover', function(event){
+	domEvents.bind(eventTarget, 'mouseover', function(event){
 		underline.visible	= true;
 		document.body.style.cursor	= 'pointer';
 	}, false)
 		
 	// bind 'mouseout'
-	domEvents.bind(mesh, 'mouseout', function(event){
+	domEvents.bind(eventTarget, 'mouseout', function(event){
 		underline.visible	= false;		
 		document.body.style.cursor	= 'default';
 	}, false)
+	
+	this.destroy	= function(){
+		console.log('not yet implemented')
+	}
 }
