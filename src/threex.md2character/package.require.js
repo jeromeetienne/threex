51f05a8ff1d2c92691d7760d796807038bc898ac
Jerome Etienne <jerome.etienne@gmail.com>
2014-04-26 13:27:54 +0200

    moved threex.md2character in its own repo

--- src/threex.md2character/package.require.js
+++
@@ -1,8 +0,0 @@
-define( [ 'module'	// to set .baseURL
-	, './threex.md2character'
-	, './threex.md2charactercontrols'
-	, './threex.md2characterratmahatta'
-	], function(module){
-	// set baseUrl for this extension
-	THREEx.MD2CharacterRatmahatta.baseUrl	= module.uri+'/../';
-});
